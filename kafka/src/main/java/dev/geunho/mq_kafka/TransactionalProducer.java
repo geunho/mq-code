fe49a101e4ad3d4abb7303d96084c089144e4cb0
Geunho Kim <geunho.dev@gmail.com>
2020-01-07 11:08:31 +0900

    Add a large message send test

--- kafka/src/main/java/dev/geunho/mq_kafka/TransactionalProducer.java
+++ kafka/src/main/java/dev/geunho/mq_kafka/TransactionalProducer.java
@@ -3,6 +3,7 @@ package dev.geunho.mq_kafka;
 import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.util.Map;
 import java.util.Properties;
 
 public class TransactionalProducer {
@@ -10,19 +11,31 @@ public class TransactionalProducer {
   private final String topic;
 
   public TransactionalProducer(String bootstrapServers, String topic) {
+    Properties props = getDefaultProperties(bootstrapServers);
+    producer = new KafkaProducer<>(props);
+    this.topic = topic;
+  }
+
+  public TransactionalProducer(String bootstrapServers, String topic, Map<Object, Object> options) {
+    Properties props = getDefaultProperties(bootstrapServers);
+    for (Object key : options.keySet()) {
+      props.put(key, options.get(key));
+    }
+    producer = new KafkaProducer<>(props);
+    this.topic = topic;
+  }
+
+  private Properties getDefaultProperties(String bootstrapServers) {
     Properties props = new Properties();
     props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
     props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
     props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
     props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true); // exactly once semantics
-    producer = new KafkaProducer<>(props);
-    this.topic = topic;
+    return props;
   }
 
   public void send(String contents) {
     Message message = new Message(contents);
-//    producer.initTransactions();
-//    producer.beginTransaction();
     producer.send(new ProducerRecord<>(topic,
         message.getId(),
         message.getContents()), (metadata, exception) -> {
